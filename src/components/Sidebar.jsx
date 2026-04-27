d332c6ed24745d90137e0b6ab7e431ce3246ef01
carlos-dev23 <jcsegurat@gmail.com>
2026-04-26 21:12:32 -0600

    feat: structure layaout with Sidebar, Resumen and category data

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <aside className="md:w-72">
+      <div className="p-4">
+        <img src="img/logo.svg" alt="" />
+      </div>
+    </aside>
+  );
+}
