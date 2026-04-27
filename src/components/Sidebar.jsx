c4ea6073b973e64cc62ca05f28707137d5e498bf
carlos-dev23 <jcsegurat@gmail.com>
2026-04-26 21:40:20 -0600

    feat: add category menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { categorias } from "../data/categories.js";
+import Categorias from "./Categoria.jsx";
 
 export default function Sidebar() {
   return (
     <aside className="md:w-72">
       <div className="p-4">
         <img src="img/logo.svg" alt="" />
+      </div>
+      <div className="mt-10">
+        {categorias.map((categoria) => (
+          <Categorias categoria={categoria} key={categoria.id} />
+        ))}
+      </div>
+
+      <div className="my-5 px-5">
+        <button type="button" className="text-center bg-red-500 w-full p-3 font-bold text-white truncate">
+          Cancelar Orden
+        </button>
       </div>
     </aside>
   );
