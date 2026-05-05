af683348acbcd0f5d5e05e50322124dd56e55a21
carlos-dev23 <jcsegurat@gmail.com>
2026-05-04 21:15:13 -0600

    feat(context) setup QuioscoProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css';
-import router from './router';
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./router";
+import QuioscoContext, { QuioscoProvider } from "./context/QuioscoProvider";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QuioscoProvider>
+      <RouterProvider router={router} />
+    </QuioscoProvider>
   </StrictMode>,
-)
+);
