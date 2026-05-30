daa65e08264795963b0e347284f78ba25bde2ba1
carlos-dev23 <jcsegurat@gmail.com>
2026-05-29 23:03:52 -0600

    feat(resumen): add order validation and disable submit button

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -2,7 +2,9 @@ import useQuiosco from "../hooks/useQuiosco";
 import ResumenProducto from "./ResumenProducto";
 import { formatearDinero } from "../helpers";
 export default function Resumen() {
-  const { pedido,total } = useQuiosco();
+  const { pedido, total } = useQuiosco();
+  
+  const comprobarPedido = () =>  pedido.length ===0;
   return (
     <aside className="w-72 h-screen overflow-y-scroll p-5">
       <h1 className="text-4xl font-black">Mi Pedido</h1>
@@ -13,7 +15,11 @@ export default function Resumen() {
       <p>Total: {formatearDinero(total)}</p>
       <form action="" className="w-full">
         <div className="mt-5">
-          <input type="submit" value="Confirmar Pedido" className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer" />
+          <input type="submit" value="Confirmar Pedido"
+            className={`${comprobarPedido() ? 'bg-indigo-100' : 'bg-indigo-600 hover:bg-indigo-800'} px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
+            disabled={comprobarPedido()}
+
+          />
         </div>
       </form>
     </aside>
