1b6bbbe9149c0acb5526642918d3017ca469b39a
carlos-dev23 <jcsegurat@gmail.com>
2026-07-01 21:45:04 -0600

    feat(inicio): create login function with API integration

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,22 +1,46 @@
 import { Link } from "react-router-dom";
+import { createRef, useState } from "react";
+import ClienteAxios from "../config/axios";
+import Alerta from "../components/Alerta";
+
 export default function Login() {
+
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const datos = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }
+    try{
+      const { data } = await ClienteAxios.post("/api/login", datos);
+      localStorage.setItem('AUTH_TOKEN', data.token);
+    }catch(error){
+      setErrors(Object.values(error.response.data.errors));
+    }
+  }
+  
   return (
     <>
       <h1>Iniciar Sesión</h1>
       <p>Para crear un pedido debes iniciar sesión</p>
       <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-        <form action="">
+        <form onSubmit={handleSubmit}>
+          {errors ? errors.map((error,i) => <Alerta key={i}>{error}</Alerta>) : null}
           <div className="mb-4">
             <label className="text-slate-800" htmlFor="email">
               Email:
             </label>
-            <input type="email" id="email" className="mt-2 w-full p-3 bg-gray-50" name="email" placeholder="Tu E-mail" />
+            <input type="email" id="email" className="mt-2 w-full p-3 bg-gray-50" name="email" placeholder="Tu E-mail" ref={emailRef} />
           </div>
           <div className="mb-4">
             <label className="text-slate-800" htmlFor="password">
               Password:
             </label>
-            <input type="password" id="password" className="mt-2 w-full p-3 bg-gray-50" name="password" placeholder="Contraseña" />
+            <input type="password" id="password" className="mt-2 w-full p-3 bg-gray-50" name="password" placeholder="Contraseña" ref={passwordRef} />
           </div>
           <input type="submit" value="Iniciar Sesión" className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer" />
         </form>
