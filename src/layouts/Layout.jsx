6edc8f05b9b3eafce1b2b8736eee2692abfeafe3
carlos-dev23 <jcsegurat@gmail.com>
2026-07-05 21:58:03 -0600

    feat(auth): configure middleware for Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,6 +5,7 @@ import Sidebar from "../components/Sidebar";
 import Resumen from "../components/Resumen";
 import ModalProducto from "../components/ModalProducto";
 import useQuiosco from "../hooks/useQuiosco";
+import { useAuth } from "../hooks/useAuth";
 
 const customStyles = {
   content: {
@@ -21,6 +22,7 @@ Modal.setAppElement("#root");
 
 export default function Layout() {
   const { modal, handleClickModal } = useQuiosco();
+  const { user, error } = useAuth({ middleware: "auth" });
 
   return (
     <>
