82b5f37f5b1ec035f49cfbc1af20493f86aa36b0
carlos-dev23 <jcsegurat@gmail.com>
2026-07-14 22:13:21 -0600

    feat(admin): add protected routes and new views for admin

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,6 +1,9 @@
 import AdminSidebar from "../components/AdminSidebar";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 export default function AdminLayout() {
+  useAuth({ middleware: "admin" });
+
   return (
    <div className="md:flex">
         <AdminSidebar />
