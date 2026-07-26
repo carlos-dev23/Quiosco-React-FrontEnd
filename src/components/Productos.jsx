a72d3acf2a376fc72422d8791d7b46fad4eb7a60
carlos-dev23 <jcsegurat@gmail.com>
2026-07-26 17:56:58 -0600

    feat(admin): implement out of stock logic and conditional buttons

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import useQuiosco from "../hooks/useQuiosco";
 
-export default function Productos({ producto }) {
-  const { handleClickModal, handleSetProducto } = useQuiosco();
+export default function Productos({ producto, botonAgregar = false, botonDisponible = false }) {
+  const { handleClickModal, handleSetProducto, handleClickProductoAgotado } = useQuiosco();
   const { nombre, imagen, precio } = producto;
   return (
     <div className="border p-3 shadow bg-white">
@@ -10,16 +10,29 @@ export default function Productos({ producto }) {
       <div className="p-5">
         <h3 className="text-2xl font-bold">{nombre}</h3>
         <p className="mt-5 font-black text-4xl text-amber-500">{precio}</p>
-        <button
-          type="button"
-          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
-          onClick={() => {
-            handleClickModal();
-            handleSetProducto(producto);
-          }}
-        >
-          Agregar
-        </button>
+        {botonAgregar && (
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+            onClick={() => {
+              handleClickModal();
+              handleSetProducto(producto);
+            }}
+          >
+            Agregar
+          </button>
+        )}
+        {botonDisponible && (
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+            onClick={() => {
+              handleClickProductoAgotado(producto.id);
+            }}
+          >
+            Producto Agotado
+          </button>
+        )}
       </div>
     </div>
   );
